ca564a81c8b90600cd8a46e6cfb6eab7b619c9b6
metallarox <elia.parussini@gmail.com>
2023-01-29 14:45:48 +0100

    Update central.ino

--- central.ino
+++ central.ino
@@ -4,17 +4,17 @@ bello e pafuttelo, della calcelcestrruzza
 
 D0 -> TX
 D1 -> RX
-D2 -> K1 APRE SX
-D3 -> K2 CHIUDE SX
-D4 -> K3 APRE DX
-D5 -> K4 CHIUDE DX
-D6 -> K5 CHIAVISTELLO
-D7 -> K6 FARETTO VIALE
+D2 -> K1 L open
+D3 -> K2 L close
+D4 -> K3 R open
+D5 -> K4 R close
+D6 -> K5 Latch
+D7 -> K6 Lamp
 D8 -> K7 NC
 D9 -> K8 NC
 D10
-D11 <- FOTOCELLULA
-D12 <- COMANDO
+D11 <- NC Light curtain 
+D12 <- NO Pulse 
 D13
 */
 
@@ -33,6 +33,32 @@ void setup() {
 }
 
 void loop() {
+  /* 
+    d12 on pulse
+    
+    OPEN
+    6 on latch
+    2 on L open
+    delay 1S
+    4 on R open
+    delay 30S
+    6 off 
+    2 off
+    4 off
+  
+    delay 60S
+    
+    CLOSE
+    6 on latch
+    3 on L close
+    delay 1S
+    5 on R close
+    delay 30S
+    6 off latch
+    3 off L close
+    5 off R close
+  
+  /*
   digitalWrite(10, HIGH);  
   delay(1000);                   
   digitalWrite(10, LOW);  
